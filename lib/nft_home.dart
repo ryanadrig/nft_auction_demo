329945634c66b507b19ae36c870d7a54a939eb21
ls <ryanadrig@gmail.com>
2023-02-17 18:31:31 -0800

    first page to collections col

--- lib/nft_home.dart
+++ lib/nft_home.dart
@@ -28,9 +28,56 @@ class _NFT_HomeState extends State<NFT_Home> {
               )
             ],
           ),
+          ),
+
+          Container(
+            width: ss.width,
+          child:ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              Container(width: ss.width * .12,
+              child: Text("Live")),
+              Container(width: ss.width * .12,
+                  child: Text("New")),
+              Container(width: ss.width * .12,
+                  child: Text("Popular"))
+            ],
+          )),
+
+          Padding(padding: EdgeInsets.symmetric(vertical: ss.width*.02),
+          child:
+              PageView.builder(itemBuilder: (context, position){
+                      return Container(
+                        child:Image.asset("assets/images/nft_asset" + position.toString())
+                      );
+              })
+          ),
+
+          Container(
+            child: Collections_Col()
           )
+
         ],),
       ),
     ));
+  }
+}
+
+
+List<Map> collections_data = [];
+
+class Collections_Col extends StatefulWidget {
+  const Collections_Col({Key? key}) : super(key: key);
+
+  @override
+  _Collections_ColState createState() => _Collections_ColState();
+}
+
+class _Collections_ColState extends State<Collections_Col> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
   }
 }
