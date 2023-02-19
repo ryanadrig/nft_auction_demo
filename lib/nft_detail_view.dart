c47b331a4e3b825a2cf121047341072d649e6459
ls <ryanadrig@gmail.com>
2023-02-18 22:48:39 -0800

    countdown timer on detail page

--- lib/nft_detail_view.dart
+++ lib/nft_detail_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:nft_auction/state/na_globals.dart';
+import 'package:flutter_countdown_timer/flutter_countdown_timer.dart';
+import 'package:nft_auction/utils.dart';
+import 'dart:ui';
 
 class NFT_Detail_View extends StatefulWidget {
    NFT_Detail_View({Key? key,
@@ -10,8 +13,12 @@ Map nft;
 }
 
 class _NFT_Detail_ViewState extends State<NFT_Detail_View> {
+  double nft_img_borderradius = 0;
+  double overlay_cd_width = 0;
   @override
   Widget build(BuildContext context) {
+    nft_img_borderradius = ss.width*.06;
+    overlay_cd_width = ss.width * .3;
     return Scaffold(body:
     SafeArea(child:Container(
       height: ss.height,
@@ -59,7 +66,81 @@ class _NFT_Detail_ViewState extends State<NFT_Detail_View> {
     ]))
     ])),
 
-              
+
+              Stack(children:[
+                Padding(
+                    padding:EdgeInsets.symmetric(horizontal:ss.width*.02),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(nft_img_borderradius),
+                        child:Image.asset(widget.nft["img_path"],
+                          height: ss.height * .6,
+                          width: ss.width*.96,
+                          fit: BoxFit.fill,))),
+                Padding(
+                    padding:EdgeInsets.symmetric(horizontal:ss.width*.02),
+                child:Positioned(
+                    top:0,
+                    child:
+                    SizedBox(
+                        height: ss.height * .08,
+                        width: overlay_cd_width,
+                        child:
+                        ClipRRect(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(nft_img_borderradius),
+                            ),
+                            child:
+                            BackdropFilter(
+                                filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 1.0),
+                                child:
+                                Container(
+                                    width: ss.width * .3,
+                                    child:Row(
+                                      children: [
+                                        Container(
+                                            height: ss.height*.18,
+                                            width: overlay_cd_width,
+                                            child:Column(
+                                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Container(
+                                                    child: CountdownTimer(endTime:
+                                                    ret_counter_endtime(
+                                                        widget.nft["auction_end"]),
+                                                      textStyle:TextStyle(
+                                                          fontSize: ss.width * .03,
+                                                          color: Colors.white))),
+
+                                                Stack(children:[
+                                                Container(
+                                                  width: ss.width* .1,
+                                                  decoration: BoxDecoration(
+                                                    border: Border(bottom:
+                                                    BorderSide(width:ss.width*.01,
+                                                    color: Colors.white
+                                                    ))
+                                                  ),
+                                                ),
+                                                  Container(
+                                                    width: ss.width* .2,
+                                                    decoration: BoxDecoration(
+                                                        border: Border(bottom:
+                                                        BorderSide(width:ss.width*.01,
+                                                            color: Colors.white54
+                                                        ))
+                                                    ),
+                                                  )
+
+                                                ])
+                                              ],)
+                                        ),
+
+                                      ],))
+                            ))
+                    ))),
+              ]
+              )
+
 
 
 
